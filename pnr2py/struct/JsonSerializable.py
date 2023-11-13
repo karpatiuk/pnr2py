5c63776a2e229ddb754d4a7f4b3f1f74df6a8b5d
Andrew Cooper <andrew.cooper@kiv.dev>
2023-11-13 11:16:57 +0200

    added docs to JsonSeializable class

--- pnr2py/struct/JsonSerializable.py
+++ pnr2py/struct/JsonSerializable.py
@@ -1,8 +1,16 @@
 import json
-from dataclasses import dataclass, field, asdict
+from dataclasses import dataclass, asdict
 
 
 @dataclass
 class JsonSerializable:
-    def to_json(self):
+    """A mixin class for JSON serialization of data classes."""
+
+    def to_json(self) -> str:
+        """
+        Serialize the data class to a JSON-formatted string.
+
+        Returns:
+            str: JSON-formatted string representing the data class.
+        """
         return json.dumps(asdict(self), indent=4)
